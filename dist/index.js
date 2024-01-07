1cd970bac126f751dcbe3840e11a25297c8e06e8
izayed9 <izayed9@gmail.com>
2024-01-07 12:21:34 +0600

    sehema setup fixed

--- dist/index.js
+++ dist/index.js
@@ -1,56 +1,46 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-
 // types
 import { typeDefs } from "./schema.js";
-
 import db from "./_db.js";
-
-
 // resolver
 const resolvers = {
-  Query: {
-    games:()=> {
-      return db.games;
+    Query: {
+        games: () => {
+            return db.games;
+        },
+        authors: () => {
+            return db.authors;
+        },
+        reviews: () => {
+            return db.reviews;
+        },
     },
-    authors:()=> {
-      return db.authors;
-    },
-    reviews:()=> {
-      return db.reviews;
-    },
-  },
 };
-
 // server setup
 const server = new ApolloServer({
-  typeDefs,
-  // Resolver
-  resolvers,
+    typeDefs,
+    // Resolver
+    resolvers,
 });
-
 // Passing an ApolloServer instance to the `startStandaloneServer` function:
 //  1. creates an Express app
 //  2. installs your ApolloServer instance as middleware
 //  3. prepares your app to handle incoming requests
 const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
+    listen: { port: 4000 },
 });
-
 console.log(`🚀  Server ready at: ${url}`);
-
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
 // const typeDefs = `#graphql
 //   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
 //   # This "Book" type defines the queryable fields for every book in our data source.
 //   type Book {
 //     title: String
 //     author: String
 //   }
-
 //   # The "Query" type is special: it lists all of the available queries that
 //   # clients can execute, along with the return type for each. In this
 //   # case, the "books" query returns an array of zero or more Books (defined above).
